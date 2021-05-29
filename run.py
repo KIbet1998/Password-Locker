db2fb8521aa9556f05646fc0d5a8a5a542e75a5d
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-29 14:59:03 +0300

    add def main

--- run.py
+++ run.py
@@ -5,3 +5,37 @@ def create_accounts(username,password):
     '''
     Function to create new account
     '''
+      new_account = User(username,password)
+    return new_account
+   #save contacts
+def save_accounts(account):
+    '''
+    method to save the user account
+    '''
+    account.save_account()
+    def del_account(account):
+    '''
+    funcion to delete account
+    '''
+    account.delete_account()
+    def main():
+    print("Hello welcome to Your password locker.What is your name?")
+    username = input()
+    print(f"Hello {username}. What would you like to do?")
+    print('\n')
+    while True:
+        print("Use these short codes: ca - create a new account,da - display account, fa - find contact, ex - exit password locker,del - delete account")
+        short_code = input().lower()
+        # print('\n')
+        if short_code == 'ca':
+            print("New User")
+            username =input()
+            print("New password")
+            password = input()
+            save_accounts(create_accounts(username,password))
+            print('\n')
+            print(f"new Account {username} {password} created")
+            print('\n')
+if __name__=='__main__':
+    main()
+
