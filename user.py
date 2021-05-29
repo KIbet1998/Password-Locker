31ab9f60e5f6229363b3a260d1a38386d8bd2028
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-29 14:36:55 +0300

    add user list

--- user.py
+++ user.py
@@ -1,7 +1,25 @@
 class User:
-    '''
+        '''
     class that generates new instace of user
     '''
+    def __init__(self,username,password):
+        '''
+        creates properties of instance of user class
+        '''
+        self.username = username
+        self.password = password
+    user_list = []
+     def save_account(self):
+        '''
+        save_account method saves a new user objects to the user list
+        '''
+        User.user_list.append(self)
+    def delete_account(self):
+        '''
+        deletes saved account
+        '''
+        User.user_list.remove(self)
+
 
 
 
