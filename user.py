50a516ef8b71a56564a04d2295dce9406d09782b
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-29 14:07:19 +0300

    add initial files

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+       def __init__(self,username,password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            username: New user username.
+            password: password.
+         
+        '''
+
