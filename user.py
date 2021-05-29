c11d3da635dd739291f8f1ebb2e905740fcfb53a
Kibet 1998 <ronoronald1998@gmail.com>
2021-05-29 14:25:49 +0300

    add constractor to initialize instance

--- user.py
+++ user.py
@@ -1,12 +1,9 @@
 class User:
-       def __init__(self,username,password):
+    '''
+    class that generates new instace of user
+    '''
 
-        '''
-        __init__ method that helps us define properties for our objects.
 
-        Args:
-            username: New user username.
-            password: password.
-         
-        '''
+
+
 
